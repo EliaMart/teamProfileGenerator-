ab667718c1cbd24d7faeb442b7777db93dba863a
Elia Martinez <eliahaydeemartinez@gmail.com>
2022-06-15 21:18:43 -0500

    added employee and manager test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -6,11 +6,33 @@ describe("Employee", () => {
             
             const name = 'Paul';
 
-            const obj = new Employee(name)
+            const obj = new Employee(name);
 
-            expect(obj.name).toEqual(name)
+            expect(obj.name).toEqual(name);
         });
 
-        it("should ")
+        it("should create an object containing an 'Id' property when called with the 'new' keyword", () => {
+            
+            const name = 'Paul';
+            
+            const Id = '1';
+
+            const obj = new Employee(name, Id);
+
+            expect(obj.Id).toEqual(Id)
+        });
+
+        it("should create an object containing an 'email' property when called with the 'new' keyword", () => {
+            
+            const name = 'Paul';
+            
+            const Id = '1';
+
+            const email = 'elia@gmail.com'
+
+            const obj = new Employee(name, Id, email);
+
+            expect(obj.email).toEqual(email)
+        });
     })
 });
---
+++ _tests_/Manager.test.js
@@ -0,0 +1,53 @@
+const Manager = require('../lib/Manager')
+
+describe("Employee", () => {
+    describe("Initialization", () => {
+        it("should create an object containing a 'name' property when called with the 'new' keyword", () => {
+            
+            const name = 'John';
+
+            const obj = new Manager(name);
+
+            expect(obj.name).toEqual(name);
+        });
+
+        it("should create an object containing an 'Id' property when called with the 'new' keyword", () => {
+            
+            const name = 'John';
+            
+            const Id = '2';
+
+            const obj = new Manager(name, Id);
+
+            expect(obj.Id).toEqual(Id)
+        });
+
+        it("should create an object containing an 'email' property when called with the 'new' keyword", () => {
+            
+            const name = 'John';
+            
+            const Id = '2';
+
+            const email = 'john@gmail.com'
+
+            const obj = new Manager(name, Id, email);
+
+            expect(obj.email).toEqual(email)
+        });
+
+        it("should create an object containing a 'office number' property when called with the 'new' keyword", () => {
+            
+            const name = 'John';
+            
+            const Id = '2';
+
+            const email = 'john@gmail.com'
+
+            const officeNumber = '1234'
+
+            const obj = new Manager(name, Id, email, officeNumber);
+
+            expect(obj.officeNumber).toEqual(officeNumber)
+        });
+    })
+});
